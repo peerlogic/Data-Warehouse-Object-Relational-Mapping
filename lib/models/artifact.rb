1622bda55fb575aa0dad4002ea28411ef85406c1
Winbobob <zhu6@ncsu.edu>
2017-04-10 23:35:07 -0400

    Add code to generate json files.

--- lib/models/artifact.rb
+++ lib/models/artifact.rb
@@ -1,5 +1,5 @@
 class Artifact < ActiveRecord::Base
-	has_many :critiques
+	has_many :critiques, foreign_key: 'assessee_artifact_id'
 	belongs_to :task, foreign_key: 'submitted_in_task_id'
 	has_many :items, foreign_key: 'reference_id'
 end
